3c228ddd8f8fd123f2bd1c238415fea9f0e68c60
Prabhu <vprabhu1771@gmail.com>
2025-02-19 12:48:22 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flutter_supabase_skill_development
+ 
---
+++ android/app/src/main/kotlin/com/example/flutter_supabase_skill_development/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_supabase_skill_development
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
